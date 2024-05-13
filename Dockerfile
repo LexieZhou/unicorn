42821ea3cdf45ea6e0f8cbb64a9e7f9c80c6f6ac
LexieZhou <zihan2@connect.hku.hk>
2024-05-13 23:06:40 +0200

    modify Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -21,17 +21,20 @@ RUN wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86
 # Put conda in path so we can use conda activate
 ENV PATH=$CONDA_DIR/bin:$PATH
 
+# Initialize conda
+RUN conda init bash
+
 # Create a new Conda environment
 RUN conda env create -f environment.yml
 
-# Activate the Conda environment
-SHELL ["conda", "run", "-n", "unicorn", "/bin/bash", "-c"]
+# # Activate the Conda environment
+# SHELL ["conda", "run", "-n", "unicorn", "/bin/bash", "-c"]
+RUN echo "conda activate unicorn" > ~/.bashrc
+ENV PATH /opt/conda/envs/unicorn/bin:$PATH
 
-# Install rembg using pip
-RUN pip install rembg
-
-# Deactivate the Conda environment
-SHELL ["conda", "run", "--no-capture-output", "-n", "base", "/bin/bash", "-c", "conda deactivate"]
+RUN /bin/bash -c "source activate unicorn \
+    && pip install rembg \
+    && conda clean -afy"
 
 # Copy the remaining files to the container
 COPY . .
--- src/reconstruct.py
+++ src/reconstruct.py
@@ -16,14 +16,16 @@ from utils.mesh import save_mesh_as_obj, normalize
 from utils.pytorch import get_torch_device
 
 
-BATCH_SIZE = 32
-N_WORKERS = 4
-PRINT_ITER = 2
-SAVE_GIF = True
+# BATCH_SIZE = 32
+# N_WORKERS = 4
+# PRINT_ITER = 2
+# SAVE_GIF = True
 warnings.filterwarnings("ignore")
 
 # furniture_type is chair or table
 def reconstruct(image_path, furniture_type):
+    BATCH_SIZE = 32
+    N_WORKERS = 4
     # Load the model and prepare it
     device = get_torch_device()
     model = furniture_type + '.pkl'
@@ -33,12 +35,15 @@ def reconstruct(image_path, furniture_type):
     # change background of the image to white
     input_img = open(image_path, 'rb').read()
     output_img = remove(input_img, bgcolor=[255, 255, 255, 255])
+
+    # save the image with no background
     noBG_folder = 'img_no_bg'
     os.makedirs(noBG_folder, exist_ok=True)
     output_path = os.path.join(noBG_folder, os.path.basename(image_path))
     with open(output_path, 'wb') as output_file:
        output_file.write(output_img)
     
+    # Load the image and reconstruct the mesh
     data = get_dataset(noBG_folder)(split='test', img_size=m.init_kwargs['img_size'])
     loader = DataLoader(data, batch_size=BATCH_SIZE, num_workers=N_WORKERS, shuffle=False)
     for j, (inp, _) in enumerate(loader):
@@ -51,12 +56,12 @@ def reconstruct(image_path, furniture_type):
 
 if __name__ == '__main__':
     print_log("Starting reconstruction...")
-    input_image_path = os.path.join("demo", 'download.jpeg')
-    reconstruction_model = reconstruct(input_image_path, 'table')
+    input_image_path = os.path.join("demo", 'chair3.jpeg')
+    reconstruction_model = reconstruct(input_image_path, 'chair')
 
     # save the result
     out = path_mkdir('demo' + '_rec')
-    name = 'table'
+    name = 'chair3'
     save_mesh_as_obj(reconstruction_model, out / f'{name}_mesh.obj')
     # if SAVE_GIF:
     #     save_mesh_as_gif(reconstruction_model, out / f'{name}_mesh.gif', n_views=100, dist=d, elev=e, renderer=m.renderer)
